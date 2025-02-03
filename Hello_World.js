8807cbdbb6346b650af1b15f951df6e4a3930be8
loganrains <logankrains@gmail.com>
2025-02-03 16:30:03 -0600

    Added Task 1b to Hello World Javascript Practice Problem

--- Hello_World.js
+++ Hello_World.js
@@ -1,3 +1,8 @@
-//Day 1 of code everyday, Back to basics
+// Task 1a - Write code that prints "Hello World!" to the terminal
 
+//Solution v
 console.log("Hello World!");
+
+
+// Task 1b - Write Code that prints the words "Hello" & "World" on two different lines using the new line character
+console.log("Hello\nWorld");
